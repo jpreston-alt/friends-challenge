89bdb1a014c3f47819986d70c56d15cd81d89c12
Joanna Preston <joannappreston@gmail.com>
2023-04-07 11:08:40 -0700

    add proptypes to Text comp

--- components/Text/Text.js
+++ components/Text/Text.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Text.module.css";
+import PropTypes from "prop-types";
 
 const tagMap = {
   h1: "h1",
@@ -12,12 +13,39 @@ const tagMap = {
   chip: "span",
 };
 
-const Text = ({ variant = "body", color = "primary", children }) => {
+const Text = ({ variant, color, children }) => {
   const Tag = tagMap[variant];
   const variantClass = styles[variant];
   const colorClass = styles[`color-${color}`];
 
   return <Tag className={`${variantClass} ${colorClass}`}>{children}</Tag>;
+};
+
+Text.propTypes = {
+  variant: PropTypes.oneOf([
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "body",
+    "label",
+    "chip",
+  ]),
+  color: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "disabled",
+    "white",
+    "blue",
+    "green",
+  ]),
+  children: PropTypes.node.isRequired,
+};
+
+Text.defaultProps = {
+  variant: "body",
+  color: "primary",
 };
 
 export default Text;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import { Text } from "@components";
+
 const Home = () => {
-  return <>home page</>;
+  return (
+    <>
+      <Text variant="h1">h1</Text>
+      <Text variant="h2" color="secondary">
+        h2
+      </Text>
+      <Text variant="h3" color="disabled">
+        h3
+      </Text>
+      <Text variant="h4" color="white">
+        h4
+      </Text>
+      <Text variant="h5" color="green">
+        h5
+      </Text>
+      <Text color="blue">body</Text>
+      <Text variant="label">label</Text>
+      <br />
+      <Text variant="chip">chip</Text>
+    </>
+  );
 };
 
 export default Home;
