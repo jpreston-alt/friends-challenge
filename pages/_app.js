0e1644e7bdc79bd568436f0a5287a7e2ca2c921c
Joanna Preston <joannappreston@gmail.com>
2023-04-11 17:34:02 -0700

    updating loading animation

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,14 @@ import { PageTemplate } from "@components";
 import "@styles/globals.css";
 
 const App = ({ Component, pageProps }) => {
+  const title = `${pageProps.title} | Clerkie`;
   return (
     <>
       <Head>
         <meta name="description" content="Clerkie friends challenge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <title>{pageProps.title} | Clerkie</title>
+        <title>{title}</title>
       </Head>
       <PageTemplate pageName={pageProps.title}>
         <Component {...pageProps} />
